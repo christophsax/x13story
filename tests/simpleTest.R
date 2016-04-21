548353db365daa4a93f97e4bd46464fce1497a74
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-04-21 23:11:37 +0200

    and the main run...

--- tests/simpleTest.R
+++ tests/simpleTest.R
@@ -3,8 +3,18 @@ seas(AirPassengers)
 
 
 if (Sys.getenv("TRAVIS") != ""){
+
+  library(x13story)
+  idir <- file.path(Sys.getenv("TRAVIS_BUILD_DIR"), "inst/stories")
   odir <- file.path(Sys.getenv("TRAVIS_BUILD_DIR"), "out")
-  write.csv(cars, file = file.path(odir, "cars.csv"))
+
+  ff <- list.files(idir, pattern = "\\.Rmd$", ignore.case = TRUE, full.names = TRUE)
+
+  STORIES <- lapply(ff, function(x) x13story::parse_x13lesson(file = x))
+
+  names(STORIES) <- gsub("(.+?)\\..+", "\\1", basename(ff))
+
+  save(STORIES, file = file.path(odir, "stories.RData"))
 }
 
 
