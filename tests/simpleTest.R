cde4581fb8f90d316791d44f46194dda5fa5a286
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-04-21 23:00:47 +0200

    writing to s3 works!

--- tests/simpleTest.R
+++ tests/simpleTest.R
@@ -1,2 +1,10 @@
 library(seasonal)
 seas(AirPassengers)
+
+
+if (Sys.getenv("TRAVIS") != ""){
+  odir <- file.path(Sys.getenv("TRAVIS_BUILD_DIR"), "out")
+  write.csv(cars, file = file.path(odir, "cars.csv"))
+}
+
+
